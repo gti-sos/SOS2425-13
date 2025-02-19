cf12be59a9cd38e16a312fb3fab1fd89f113363a
Darío López <darlopvil@alum.us.es>
2025-02-19 10:12:52 +0100

    Fixes #11: index-darlopvil.js agregado

---
+++ index-darlopvil.js
@@ -0,0 +1,20 @@
+//Funcion que imprime por consola
+function log(m) {
+    console.log(m);
+}
+
+//Declaracion del array
+let myArray = [1954, 1954, 1918, 1916, 1955, 1957, 1973, 1981, 1991, 1995];
+
+//Operacion de reduccion y media
+let sumaFechas = myArray.reduce( (acumulador, numero) => { return acumulador + numero; }, 0);
+let mediaFechas = sumaFechas / myArray.length;
+
+//Imprimiendo resultados
+log("Imprimiendo el array: " +myArray);
+log("Imprimiendo media de las fechas: " +mediaFechas);
+
+
+
+
+
