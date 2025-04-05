0a594efa689175a5010fb2aa385c7b210c644270
Dario Lopez <darlopvil@alum.us.es>
2025-04-05 16:52:31 +0200

    darlopvil push to F06

--- index.js
+++ index.js
@@ -6,19 +6,6 @@ app.use("/", express.static("public"));
 app.use(express.json());
 
 
-//Acceder al homepage lanza un index.html de forma estatica
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "./public/index.html");
-});
-
-//Acceder al homepage lanza el about.html de forma estatica
-app.get("/about", (req, res) => {
-    res.sendFile(__dirname + "./public/about.html");
-});
-
-
-
-
 // ---- PARTE BLANCA (modularizada correctamente) ----
 import { loadBackend } from "./src/back_D01/index-BGA.js";
 loadBackend(app);
