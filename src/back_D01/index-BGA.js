f7bd4aec077d459c016552099335d923ea39c1a8
Blanca <116111975+blancagrclns@users.noreply.github.com>
2025-04-06 15:40:42 +0200

    Update index-BGA.js

--- src/back_D01/index-BGA.js
+++ src/back_D01/index-BGA.js
@@ -52,7 +52,7 @@ const datosInicialesB = [
     { year: 2017, autonomous_community: "pais vasco", amount: 3194418, benefited_population: 10317, project_count: 18}
 ];
 
-function loadBackend(app) {
+function loadBackendB(app) {
     // Load Initial Data
     app.get(BASE_API + "/water-supply-improvements/loadInitialData", (req, res) => {
         db.count({}, (err, count) => {
