2317bfad9db5253fbb985fcda29b1f9ba92c1a9e
Blanca García Alonso <blancagarcialonso@gmail.com>
2025-04-14 14:45:20 +0200

    Todo el video L07

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 16078;
 
-import {handler} from "src/GUI_D02/build/handler.js";
+import {handler} from "./src/GUI_D02/build/handler.js";
 
 
 //app.use("/", express.static("public"));
