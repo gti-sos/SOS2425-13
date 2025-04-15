f7bd5615f29a2e970c70430d1286b2ec6f2ab34b
Blanca García Alonso <blancagarcialonso@gmail.com>
2025-04-15 14:55:58 +0200

    nuevo front entero con datos de cada integrante

---
+++ front/src/routes/+page.svelte
@@ -0,0 +1,90 @@
+<main>
+    <h1><u>SOS2425-13</u></h1>
+    <h2>Descripción del proyecto</h2>
+    <p style="font-size: 20px;">
+        Nuestro proyecto se basa en la integración de datos sobre incendios forestales, parques naturales y ayudas para el abastecimiento de agua, con el objetivo de analizar la relación entre la disponibilidad de este recurso y su impacto en la gestión ambiental y social. A través de esta unión de información, buscamos identificar patrones que permitan mejorar la prevención y respuesta ante incendios, optimizar la conservación de ecosistemas en áreas protegidas y evaluar la efectividad de los subsidios destinados a mitigar la escasez hídrica en municipios vulnerables. Este enfoque integral contribuirá al desarrollo de estrategias más eficientes y sostenibles en la gestión del agua como recurso esencial.
+
+    </p>
+    <br>
+    <h2>Equipo</h2>
+    <ul>
+        <li>
+            <a href="https://github.com/blancagrclns"> Blanca García Alonso (water-supply-improvements)</a>
+        </li>
+        <li>
+            <a href="https://github.com/darlopvil">Darío López Villegas (national-parks)</a>
+        </li>
+        <li>
+            <a href="https://github.com/alvmornav">Alvaro Morillo Navajas (forest-fires)</a>
+        </li>
+    </ul>
+    <h2>Repositorio</h2>
+    <a href="https://github.com/tu-equipo/SOS2425-13" class="margen">GitHub del equipo: SOS2425-13</a>
+    <br>
+    <br>
+    <h2>Documentación Postman</h2>
+    <ul>
+        <li>
+            <a href="https://documenter.getpostman.com/view/42334859/2sB2cVe2Fy">Documentación API Blanca García Alonso</a>
+        </li>
+        <li>
+            <a href="https://documenter.getpostman.com/view/14944672/2sB2cUBNyt">Documentación API Darío López Villegas</a>
+        </li>
+        <li>
+            <a href="https://documenter.getpostman.com/view/42116184/2sB2cUBNgF">Documentación API Alvaro Morillo Navajas</a>
+        </li>
+    </ul>
+    <br>
+    <h2>APIs</h2>
+    <ul>
+        <li>
+            <a href="http://sos2425-13.onrender.com/api/v1/water-supply-improvements">API Blanca García Alonso</a>
+        </li>
+        <li>
+            <a href="http://sos2425-13.onrender.com/api/v1/national-parks">API Darío López Villegas</a>
+        </li>
+        <li>
+            <a href="http://sos2425-13.onrender.com/api/v1/forest-fires">API Alvaro Morillo Navajas</a>
+        </li>
+    </ul>
+    <br>
+    <h2>FrontEnd</h2>
+    <ul>
+        <li>
+            <a href="http://sos2425-XX.onrender.com/frontend-water-supply-improvements">FrontEnd Blanca García Alonso</a>
+        </li>
+        <li>
+            <a href="http://sos2425-13.onrender.com/frontend-national-parks">FrontEnd Darío López Villegas</a>
+        </li>
+        <li>
+            <a href="http://sos2425-13.onrender.com/frontend-forest-fires">FrontEnd Alvaro Morillo Navajas</a>
+        </li>
+    </ul>
+</main>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+    h2 {
+        margin-left: 20px;
+        font-size: 25px;
+    }
+    p {
+        margin-left: 20px;
+    }
+    li {
+        margin-top: 10px;
+    }
+    main {
+        margin-left: 40px;
+    }
+    a {
+        text-decoration: none;
+        font-size: 20px;
+        color: rgb(0, 115, 255);
+    }
+    .margen {
+        margin-left: 45px;
+    }
+</style>
