21b9a6269910c8f3014b3cb5f7b124884401e99c
Blanca García Alonso <blancagarcialonso@gmail.com>
2025-04-19 13:14:53 +0200

    icono

--- front/src/routes/Footer.svelte
+++ front/src/routes/Footer.svelte
@@ -1,13 +1,25 @@
-<main>
-    
-    <a href="https://github.com/gti-sos/SOS2425-13" style="text-decoration: none;">Realizado por SOS2425-13 </a>
-</main>
-<style>
-    main{
-        margin-bottom: 30px;
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Mi Sitio Web</title>
+  <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+</head>
+<body>
+
+  <main>
+    <a href="https://github.com/gti-sos/SOS2425-13" style="text-decoration: none;">Realizado por SOS2425-13</a>
+  </main>
+
+  <style>
+    main {
+      margin-bottom: 30px;
     }
-    a{
-        color:rgb(2, 0, 8);
-        margin-left: 40px;
+    a {
+      color: rgb(2, 0, 8);
+      margin-left: 40px;
     }
-</style>
+  </style>
+
+</body>
+</html>
