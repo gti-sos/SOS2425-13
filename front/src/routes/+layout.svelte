751bbf0e5a25209f191f271cbb8ddcf1cfd5b0b7
Dario Lopez <darlopvil@alum.us.es>
2025-04-20 18:34:46 +0200

    fix GA

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import Header from "./Header.svelte";
     import Footer from "./Footer.svelte";
 </script>
-<link rel="preload" href="/_app/immutable/assets/0.Ayp-8-GK.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+
 
 <main>
     <Header/>
