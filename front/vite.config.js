aa00c828377774001fcbb42da3bccaaf76cb3576
Blanca <116111975+blancagrclns@users.noreply.github.com>
2025-04-21 11:58:24 +0200

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,8 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-    port: 5173,  // Establece el puerto 5173 para el servidor de desarrollo
-  },
+	plugins: [sveltekit()]
 });
